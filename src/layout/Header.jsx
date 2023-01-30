b03241b4148b927842cf2c90e3f4e9d6db706e9b
Albe390 <78861941+Albe390@users.noreply.github.com>
2023-01-30 18:36:39 +0300

    project complete

---
+++ src/layout/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export function Header() {
+  return (
+   <nav className='deep-purple darken-1'>
+   <div className="nav-wrapper">
+     <a href="#" className="brand-logo">React movies</a>
+     <ul id="nav-mobile" className="right hide-on-med-and-down">
+       <li><a href="!#">Repo</a></li>
+       
+     </ul>
+   </div>
+ </nav>
+  )
+}
